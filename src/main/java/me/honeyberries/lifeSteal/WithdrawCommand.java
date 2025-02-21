10c57d45d3baf9920588a901c10e1779275166a9
Henry <henry.rainbowfish@gmail.com>
2025-02-20 22:47:40 -0800

    Updated to Adventure API for chat messages and custom heart name

--- src/main/java/me/honeyberries/lifeSteal/WithdrawCommand.java
+++ src/main/java/me/honeyberries/lifeSteal/WithdrawCommand.java
@@ -10,7 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
 import java.util.HashMap;
 import java.util.List;
 
